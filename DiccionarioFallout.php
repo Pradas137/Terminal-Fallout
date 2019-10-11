5361d86d90bcb09d153f0a84d1766c77fc9a503a
Ruben <48710134+RubenHCCH@users.noreply.github.com>
2019-10-11 16:17:44 +0200

    refactor
    add return, quitar estructura html, quitar variables no usadas

--- DiccionarioFallout.php
+++ DiccionarioFallout.php
@@ -1,27 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
 <?php
 $archivo = fopen('DiccionarioFallout.txt','r');
 while ($linea = fgets($archivo)) {
     $aux[] = $linea;
-    $numlinea++;
 }
 fclose($archivo);
-echo '<pre>';
-print_r($aux);
-echo '</pre>';
+return $aux;
 ?>
-
-
-
-</head>
-<body>
-
-
-</body>
-</html>
-
-
-
-

618ba32fd9c42a362691a1e8b13584f77fc3ffe5
Ferran Herrero Soto <41327308+ferrysoto@users.noreply.github.com>
2019-10-14 20:22:44 +0200

    Delete wrong file

--- PalabraClave.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-$claves = array_rand($aux);
-$aux[$claves]."\n";
-
-require 'DiccionarioFallout.php';
-$pass = $aux[$claves];
-echo "<input type='hidden' id='password' value=\"$pass\">"
-?>

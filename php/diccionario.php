4f4338e9f20ce5e18a39ac8b740fcec7782b9caf
Ferran Herrero Soto <41327308+ferrysoto@users.noreply.github.com>
2019-10-14 19:33:19 +0200

    Rename source

--- php/diccionario.php
+++ php/diccionario.php
@@ -1,6 +1,6 @@
 
 <?php
-    $archivo = fopen('resources/diccionarioFallout.txt','r');
+    $archivo = fopen('resources/diccionario.txt','r');
     while ($linea = fgets($archivo)) {
         $palabras[] = trim(strtoupper($linea));
     }

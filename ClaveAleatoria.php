b38a100810b5f6f64762985011dc77e127ba7d13
Pradas137 <pradas137@gmail.com>
2019-10-14 12:05:07 +0200

    clave

--- ClaveAleatoria.php
+++ ClaveAleatoria.php
@@ -1,5 +1,28 @@
 <?php
-$claves = array_rand($arrayPalabras);
-echo $arrayPalabras[$claves];
+$archivo = fopen('diccionarioNukacola.txt','r');
+while ($linea = fgets($archivo)) {
+    $aux[] = trim($linea);
+}
+fclose($archivo);
+//print_r($aux);
+?>
+
+<?php
+
+function array_random_assoc($arr, $num = 1) {
+    $keys = array_keys($arr);
+    shuffle($keys);
+   
+    $r = array();
+    for ($i = 0; $i < $num; $i++) {
+        $r[$keys[$i]] = $arr[$keys[$i]];
+    }
+    return $r;
+}
+
+print_r(array_random_assoc($aux));
+
 
 ?>
+
+

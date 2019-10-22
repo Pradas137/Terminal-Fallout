e1564669fea5d048c06916e5f323b3bdfa05bca4
Ruben Hernandez <rubenhc99@gmail.com>
2019-10-22 16:28:43 +0200

    29-30 Ayudas

--- php/game.php
+++ php/game.php
@@ -9,6 +9,14 @@
     <title>Fallout Hack Terminal</title>
   </head>
   <body class="background">
+    <!-- only for test purposes -->
+    <!-- <script src="../js/test.js"></script> -->
+    <header>
+      <h3>Tests:</h3>
+      <button id='testwin'>win</button><button id="testlose">lose</button><br>
+      <span id="passtest"></span>
+    </header>
+    <!--  -->
     <div id="container" class="terminal">
       <div id="gamePanel">
         <p id="title">ROBCO INDUSTRIES (TM) TERMINAL PROTOCOL</p>
@@ -40,10 +48,5 @@
         lose
       </div>
     </div>
-    <!-- only for test purposes -->
-    <footer>
-      <h3>Tests:</h3>
-      <button id='testwin'>win</button><button id="testlose">lose</button>
-    </footer>
   </body>
 </html>
